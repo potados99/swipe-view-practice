cd880b77e07b36d9e91af2dee4a2fa0fed7e21e1
potados99 <potados99@gmail.com>
2020-09-17 07:40:27 +0900

    XML cleaned.

--- app/src/main/java/org/potados/swipeviewpractice/SectionAdapter.kt
+++ app/src/main/java/org/potados/swipeviewpractice/SectionAdapter.kt
@@ -3,6 +3,7 @@ package org.potados.swipeviewpractice
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.PagerSnapHelper
 import androidx.recyclerview.widget.RecyclerView
@@ -54,6 +55,10 @@ class SectionAdapter : RecyclerView.Adapter<SectionAdapter.SectionViewHolder>() 
         fun bind(content: String) {
             with(itemView) {
                 section_name.text = content
+
+                more_button.setOnClickListener {
+                    Toast.makeText(context, "Not implemented!", Toast.LENGTH_SHORT).show()
+                }
             }
 
             propStackAdapter.wholeProps = listOf("Hello", "World", "Lorem", "Ipsum", "Dolor")
